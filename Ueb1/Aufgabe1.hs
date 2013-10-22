7da3d5b7e0cb014d4a332e0fba9e2779a9ab4d99
Julian <jutanke@googlemail.com>
2013-10-22 17:34:41 +0200

    bla

---
+++ Ueb1/Aufgabe1.hs
@@ -0,0 +1,17 @@
+import Data.List
+-- Aufgabe 1
+ispyth (x,y,z) = x*x+y*y == z*z
+
+-- Aufgabe 2
+isleap j | 0==j`mod`100 = 0 == j`mod`400
+	   | otherwise    = 0 == j`mod`4
+	   
+-- Aufgabe 3
+isInt x = x == fromInteger (round x)
+ismulti (a,b,c) = isInt(s!!2/s!!1) && isInt(s!!2/s!!0) where s = sort[a,b,c]
+	
+-- Aufgabe 4
+toAscii :: Char -> Int
+toAscii s = fromEnum s
+
+toLower s = if 
