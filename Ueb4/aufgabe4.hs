f01a126cb4299a869080907cdcfc74cf052697e5
justayak <justayak@googlemail.com>
2013-11-16 16:06:29 +0100

    prim

--- Ueb4/aufgabe4.hs
+++ Ueb4/aufgabe4.hs
@@ -5,3 +5,11 @@ listOfSums a = step a a [] where
 	step list (x:xs) acc = step list xs (acc ++ [i + x| i<-list])
 	step list [] acc = acc
 -- Aufgabe1 b
+primesTo::Int->[Int]
+primesTo n = filter isPrime [2 .. n] where 
+	isPrime n = go 2
+	  where
+		go d
+		  | d*d > n        = True
+		  | n `rem` d == 0 = False
+		  | otherwise      = go (d+1)
