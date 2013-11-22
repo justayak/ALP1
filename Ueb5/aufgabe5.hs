e18b54429f59c49dac2575293c73672a014efad9
justayak <justayak@googlemail.com>
2013-11-22 14:29:29 +0100

    a5

---
+++ Ueb5/aufgabe5.hs
@@ -0,0 +1,30 @@
+-- Aufgabe 1a
+--longestRepSeq::(Ord a)=>[a]->[a]
+--longestRepSeq
+
+
+
+-- Aufgabe 2a
+flatten::[[a]]->[a]
+flatten l = foldr (++) [] l
+-- Aufgabe 2b
+flattenL::[[a]]->[a]
+flattenL l = foldl (++) [] l
+-- Aufgabe 2c
+-- foldr ist besser, da die Funktionswerte nicht vertauscht
+-- werden müssen (foldr f ist besser als foldl (flip f))
+-- Außerdem kommt foldr dank lazy ev. mit unendlichen Listen
+-- besser klar 
+-- Aufgabe 3
+bin2dec::[Int]->Int
+bin2dec = foldl1 ( (+) . (*10))
+
+temp::[Int]->Int
+temp l = foldl (+) 2 l
+
+-- Aufgabe 4
+majority :: (Eq a) => [a] -> Maybe a
+majority l | (length l) > 5 = Just (l !! 0)
+	| otherwise = Nothing where 
+		half = ( (length l) `div` 2) + 1;
+		
