4f7c1f61e4fe18a89d84a00bc66fe2e915927138
justayak <justayak@googlemail.com>
2013-12-01 17:22:30 +0100

    a1

--- Ueb6/aufgabe6.hs
+++ Ueb6/aufgabe6.hs
@@ -1,5 +1,24 @@
 import QRationals
-
--- Aufgabe 1
+-- Aufgabe 1a/b
 ngerade::Nat->Bool
-ngerade n = True
+ngerade n = (nat2Int n) `mod`2 == 0
+nmax::Nat->Nat->Nat
+nmax a b | ai > bi = a
+	|otherwise = b 
+	where ai = nat2Int a;
+		bi = nat2Int b;
+nat2Int::Nat->Int
+nat2Int n = countLetters (show n) 'S' where
+	countLetters::String -> Char -> Int
+	countLetters str c = length $ filter (==c) str
+int2Nat::Integer->Nat
+int2Nat i | i == 0 = Zero
+	| otherwise = add one (int2Nat(i-1))
+-- Aufgabe 1c/d
+zint2Int::ZInt->Int
+zint2Int (Z Zero a) = nat2Int a
+zint2Int (Z a Zero) = (nat2Int a) * (-1)
+
+int2Zint::Integer->ZInt
+int2Zint i | i >= 0 = (Z Zero (int2Nat(i)))
+	| otherwise = (Z (int2Nat(-i)) Zero)
